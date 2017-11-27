3bd73012928e6731d6b36f5f190a7ccac27c000f
jjkusowski <jjkusowski@yahoo.com>
2017-11-27 15:38:16 -0800

    add function complete and tested.

--- lab-jeff/__test__/arithmetic.test.js
+++ lab-jeff/__test__/arithmetic.test.js
@@ -6,8 +6,12 @@ describe('arithmetic.test.js', () => {
   describe('arithmetic.add', () => {
     test('arithmetic.add(a, b) should return the sum of a and b', () => {
       expect(arithmetic.add(1, 2)).toEqual(3);
+      expect(arithmetic.add(5, 10)).toEqual(15);
     });
-
+    test('if either parameter is not a number, arithmetic.add() should return null', () => {
+      expect(arithmetic.add('hi', 1)).toEqual(null);
+      expect(arithmetic.add(2, true)).toEqual(null);
+    });
 
   });
 });
--- lab-jeff/lib/arithmetic.js
+++ lab-jeff/lib/arithmetic.js
@@ -3,5 +3,6 @@
 const Arithmetic = module.exports = {};
 
 Arithmetic.add = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') return null;
   return a + b;
 };
