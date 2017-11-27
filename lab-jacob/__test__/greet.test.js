e97ed7374c3acb834baaff55d00ff985b5141e29
Jacob M-G Evans <jacobmgevans@gmail.com>
2017-11-27 15:27:32 -0800

    untracked file added

--- lab-jacob/__test__/greet.test.js
+++ lab-jacob/__test__/greet.test.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const greet = require(../lib/greet);
 
 describe('greet.js test', () => {
+  test('should return hello ${name}', () => {
+    expect(greet.hello('name').toEqual('hello name'))
+  })
 
 
 });
--- lab-jacob/lib/greet.js
+++ lab-jacob/lib/greet.js
@@ -1,2 +1,4 @@
 'use strict';
 
+const Greet = module.export = {};
+
