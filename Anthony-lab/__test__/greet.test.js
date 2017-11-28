c270c54fa524e5716ffdc3cf539af1996b8c67e5
Anthony Robinson <robinsonamk@gmail.com>
2017-11-28 09:07:26 -0800

    removed unnecessary comments

--- Anthony-lab/__test__/greet.test.js
+++ Anthony-lab/__test__/greet.test.js
@@ -7,25 +7,21 @@ describe('Testing the greet functionality', () => {
   test('greet() should return null', () => {
     let result = greet();
     expect(result).toEqual(null);
-    console.log('null');
   });
 
   test('greet() should return "hello World"', () => {
     let result = greet('World');
     expect(result).toEqual('hello World');
-    console.log('anthony');
   });
 
   // Extra Tests
   test('greet() should return "hello Anthony"', () => {
     let result = greet('Anthony');
     expect(result).toEqual('hello Anthony');
-    console.log('anthony');
   });
 
   test('greet() should return null', () => {
     let result = greet(123);
     expect(result).toEqual(null);
-    console.log('number');
   });
 });
