686862bc8294785db30e8106eb9de3991399f1a6
SethDonohue <donohue.seth@gmail.com>
2017-11-27 16:17:43 -0800

    added README.md

--- lab-seth/__test__/arithmetic.test.js
+++ lab-seth/__test__/arithmetic.test.js
@@ -4,7 +4,6 @@ const arithmetic = require('../lib/arithmetic.js');
 
 describe('arithmetic.test.js', () => {
   describe('arithmetic.add', () => {
-
     test('arithmetic.add should return sum of the inputs numbers if no errors are present', () => {
       expect(arithmetic.add(1, 2)).toEqual(3);
       expect(arithmetic.add('stuff')).toEqual(null);
@@ -12,11 +11,9 @@ describe('arithmetic.test.js', () => {
   });
 
   describe('arithmetic.sub', () => {
-
     test('arithmetic.sub should return input2 - input1 if no errors are present', () => {
       expect(arithmetic.sub(1, 2)).toEqual(1);
       expect(arithmetic.sub('stuff')).toEqual(null);
     });
-
   });
 });
