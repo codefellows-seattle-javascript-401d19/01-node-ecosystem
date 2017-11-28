094981d59fc22dccf111ade62f29d98d8c000c3f
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-11-27 16:08:31 -0800

    write and pass tests for arithmetic.sub

--- lab-shannon/lib/arithmetic.js
+++ lab-shannon/lib/arithmetic.js
@@ -9,3 +9,10 @@ Arithmetic.add = function(num1, num2){
 
   return num1 + num2;
 }
+
+Arithmetic.sub = function(num1, num2){
+  if (typeof num1 !== `number` || typeof num2 !== `number`){
+    return `null`;
+  }
+  return num1 - num2;
+}
--- lab-shannon/test/arithmetic.test.js
+++ lab-shannon/test/arithmetic.test.js
@@ -3,12 +3,20 @@
 const arithmetic = require(`../lib/arithmetic`);
 
 describe(`test the arithmetic functions`, () => {
-  describe(`the arithmetic.add function should return the sum of the arguments provided`, () => {
-    test(`the arithmetic.add function should return the sum of the arguments provided`, () => {
+  describe(`the arithmetic.add function should return the sum of the arguments provided if both arguments are valid numbers`, () => {
+    test(`the arithmetic.add function should return the sum of the arguments provided if both arguments are valid numbers`, () => {
       expect(arithmetic.add(2,7)).toBe(9);
     })
-    test(`the arithmetic.add function should return null if one or more of the arguments provided is not a number`, () => {
+    test(`the arithmetic.add function should return 'null' if one or more of the arguments provided is not a number`, () => {
       expect(arithmetic.add(`pie`, 2)).toBe(`null`);
+    })
+  })
+  describe(`the arithmetic.sub function should return the difference between the arguments provided if both arguments are valid numbers`, () => {
+    test(`the arithmetic.sub function should return the difference between the arguments provided if both arguments are valid numbers`, () => {
+      expect(arithmetic.sub(9,4)).toBe(5);
+    })
+    test(`the arithmetic.sub function should return 'null' if one or more of the arguments provided is not a number`, () => {
+      expect(arithmetic.sub(`potato`, 7)).toBe(`null`);
     })
   })
 });
