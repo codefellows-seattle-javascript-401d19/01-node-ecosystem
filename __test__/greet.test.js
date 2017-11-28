038db071b3f2d864ec4a267678ba7940370f76da
David Lindahl <davidalindahl@gmail.com>
2017-11-27 16:46:44 -0800

    greet tests fully working. woot!

--- __test__/greet.test.js
+++ __test__/greet.test.js
@@ -5,12 +5,12 @@ const greet = require('../lib/greet.js')
 describe('greet.test.js', () => {
     describe('greet.hi', () => {
         
-        test('greet.hi should return "hello world" if no errors are present', () => {
-            expect(greet.hi).toEqual('Hello world');
+        test('greet.hi should return "hello <name>" if no errors are present', () => {
+            expect(greet.hi('fred')).toEqual('hello fred');
     });
 
-        test('return null when user supplies non string values', () => {
-            expect(greet.hi).toBeNull('');
+        test('greet.hi should return null when user supplies non string values', () => {
+            expect(greet.hi('')).toEqual('null');
         });
     });
 })
