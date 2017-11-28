9c7c472a4972ed2b68bc07420c77dbac01dd3603
Dalton Carr <daltonlcarr@yahoo.com>
2017-11-27 20:01:07 -0800

    initial commit

---
+++ tests/greet.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const greet = require('../lib/greet');
+
+describe('greet.test.js', () => {
+    describe('greet.hi', () => {
+  
+      test('greet.hi should return “Hello, <name>!” if no errors are present', () => {
+        // vinicio - a test should be as small as possible
+        expect(greet.hi('Dalton')).toEqual('Hello, Dalton!');
+        expect(greet.hi('Carissa')).toEqual('Hello, Carissa!');
+      });
+  
+      test('-1 should be returned in case of error', () => {
+        expect(greet.hi('')).toEqual(-1);
+      });
+  
+    });
+    describe('greet.bye', () => {
+  
+      test('greet.bye should return “Goodbye, <name>!” if no errors are present', () => {
+        expect(greet.bye('Dalton')).toEqual('Goodbye, Dalton!');
+        expect(greet.bye('Carissa')).toEqual('Goodbye, Carissa!');
+      });
+  
+      test('-1 should be returned in case of error', () => {
+        expect(greet.bye('')).toEqual(-1);
+
+      });
+  
+    });
+  
+  });
