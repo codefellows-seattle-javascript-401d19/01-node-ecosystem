95688242f3a10f039c10a94d622e4b2eae976638
David Lindahl <davidalindahl@gmail.com>
2017-11-27 17:39:06 -0800

    finishing touches

--- __test__/arithmetic.test.js
+++ __test__/arithmetic.test.js
@@ -13,4 +13,15 @@ describe('arithmetic.test.js', () => {
             expect(arithmetic.add('')).toEqual('null');
         });
     })
+
+    describe('arithmetic.sub', () => {
+        
+                test('arithmetic.sub should return the difference between two numbers', () => {
+                    expect(arithmetic.sub(1, 1)).toEqual(0);
+                });
+        
+                test('arithmetic.sub should return null if either parameter is a non-number', () => {
+                    expect(arithmetic.sub('')).toEqual('null');
+                });
+            })
 })
