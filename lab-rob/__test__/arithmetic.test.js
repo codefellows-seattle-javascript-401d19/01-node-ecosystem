2eaa948d610378d7fb239f4ea35b6ef056da1d4c
Robert Reed <robert.mc.reed@gmail.com>
2017-11-27 17:48:00 -0800

    arithmetic helper function negative tests pass

--- lab-rob/__test__/arithmetic.test.js
+++ lab-rob/__test__/arithmetic.test.js
@@ -3,7 +3,11 @@
 const arithmetic = require('../lib/arithmetic');
 
 describe('Helper functions should return a boolean.', () => {
-  test('areNums(array) should return true if all elements in the array are numbers', () => {
+  test('areNums(arry) should return true if all elements in the array are numbers', () => {
     expect(arithmetic.areNums([1, 2, 3])).toBeTruthy();
+  });
+  test('areNums(arry) should return false if any element in the array is not a number, or if the array is empty.', () => {
+    expect(arithmetic.areNums([1, 2, 'hey'])).toBeFalsy();
+    expect(arithmetic.areNums([])).toBeFalsy();
   });
 });
--- lab-rob/lib/arithmetic.js
+++ lab-rob/lib/arithmetic.js
@@ -4,6 +4,6 @@ const arithmetic = module.exports = {};
 
 arithmetic.areNums = arry => {
   for(let i = 0; i < arry.length; i++)
-    if(isNaN(arry[i])) return false;
-  return true;
+    if(typeof arry[i] !== 'number') return false;
+  return arry.length > 0 ? true : false;
 };
