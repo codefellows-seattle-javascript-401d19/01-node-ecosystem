35b92bdf65199419253b6d318fab1a01a809c199
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-31 10:15:18 -0800

    added tests to greet.js for edge cases

--- lab-cameron/__test__/greet.test.js
+++ lab-cameron/__test__/greet.test.js
@@ -3,7 +3,15 @@
 const greet = require('../lib/greet').greet;
 
 describe('greet.js', () => {
-  test('greet.js should return "Hello, world!"', () => {
+  test('greet(<string>) should return "Hello, <string>!"', () => {
     expect(greet('world')).toBe('Hello, world!');
+  });
+
+  test('greet(<empty-string>) should return "Hello, !"', () => {
+    expect(greet('')).toBe('Hello, !');
+  });
+
+  test('greet(<number>) should return null', () => {
+    expect(greet(5)).toBe(null);
   });
 });
