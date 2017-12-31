61dc57444e684b723013b4d9f638d74cc8587de0
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-31 09:58:09 -0800

    put describe blocks around arithmetic.js sub and add functions for readability

--- lab-cameron/__test__/arithmetic.test.js
+++ lab-cameron/__test__/arithmetic.test.js
@@ -4,23 +4,27 @@ const add = require('../lib/arithmetic').add;
 const sub = require('../lib/arithmetic').sub;
 
 describe('arithmetic.js', () => {
-  test('add(2,3) should return 5', () => {
-    expect(add(2,3)).toBe(5);
+  describe('add(<number>, <number>)', () => {
+    test('add(2,3) should return 5', () => {
+      expect(add(2,3)).toBe(5);
+    });
+
+    test('add(<string>, <number>) should return null', () => {
+      expect(add('string', 5)).toBe(null);
+    });
+
+    test('add("string") should return null', () => {
+      expect(add('string')).toBe(null);
+    });
   });
 
-  test('add(<string>, <number>) should return null', () => {
-    expect(add('string', 5)).toBe(null);
-  });
+  describe('sub(<number>, <number>)', () => {
+    test('sub(1,2) should return -1', () => {
+      expect(sub(1,2)).toBe(-1);
+    });
 
-  test('add("string") should return null', () => {
-    expect(add('string')).toBe(null);
-  });
-
-  test('sub(1,2) should return -1', () => {
-    expect(sub(1,2)).toBe(-1);
-  });
-
-  test('sub("string") should return null', () => {
-    expect(sub('string')).toBe(null);
+    test('sub("string") should return null', () => {
+      expect(sub('string')).toBe(null);
+    });
   });
 });
